856a842b0e165d81dfd9d6f240e004cc11ffe477
Max <max.burlutsky@gmail.com>
2025-05-21 13:54:54 +0300

    Add Homework 08.

---
+++ HW08_Haifa-guide/js/haifa-guide.js
@@ -0,0 +1,107 @@
+const details = [
+    {
+        title: "The Baha'i World Center",
+        src: "images/Bahai.jpg",
+        src_thumb: "images/thumb/Bahai_thumb.jpg",
+        alt: "Baha'i World Center",
+        description: "<h2>The Bahá’í World Centre</h2>\n" +
+            '            <p>The\n' +
+            '                <a href="https://www.bwc.org/" target="_blank">\n' +
+            '                    <cite title="The international website of the Bahá’ís of the world.">Bahá’í World Centre</cite></a>,\n' +
+            '                the spiritual and administrative heart of the Bahá’í community, is located in\n' +
+            '                the twin cities of ‘Akká and Haifa in northern Israel. It comprises the Shrines of Bahá’u’lláh, the Báb,\n' +
+            '                and ‘Abdu’l-Bahá as well as other holy sites in the surrounding area.\n' +
+            '            </p>\n' +
+            '            <p>\n' +
+            '                In the vicinity of the Shrine of the Báb there are a number of structures including the Seat of the\n' +
+            '                Universal House of Justice, the International Teaching Centre Building, the Centre for the Study of the\n' +
+            '                Texts and the International Bahá’í Archives, all of which are set in extensive gardens.\n' +
+            '            </p>',
+    },
+    {
+        title: "The Stella Maris Monastery",
+        src: "images/StellaMaris.jpg",
+        src_thumb: "images/thumb/StellaMaris_thumb.jpg",
+        alt: "The Stella Maris Monastery",
+        description: "<h2>The Stella Maris Monastery</h2>\n" +
+            "            <p>The Stella Maris Monastery is a Catholic Christian monastery for Discalced Carmelite monks, " +
+            "located on the slopes of Mount Carmel in Haifa, Israel.</p>" +
+            "            <p>The main church inside the Stella Maris Monastery is said to contain the Cave of Elijah, " +
+            "a grotto associated with the Biblical prophet Elijah. For centuries, it has been a destination " +
+            "for Christian, Muslim and Druze pilgrims.</p>" +
+            "            <p>It is also known as the Monastery of Our Lady of Mount Carmel for monks, to distinguish " +
+            "it from the nearby Monastery of Our Lady of Mount Carmel for nuns, located higher up on Mount Carmel.</p>",
+    },
+    {
+        title: "The Louis Promenade",
+        src: "images/LouisPromenade.jpg",
+        src_thumb: "images/thumb/LouisPromenade_thumb.jpg",
+        alt: "The Louis Promenade",
+        description: '<h2>The Louis Promenade</h2>\n' +
+            "            <p>One of the loveliest places to walk and see in all of Haifa is the Louis Promenade " +
+            "on Mount Carmel. The promenade is conveniently located minutes away from numerous museums, shops " +
+            "and several hotels such as the Dan Panorama, the Dan Carmel and the Nof Hotel. This pleasantly peaceful " +
+            "promenade etched onto the slope of Haifa’s mountain is perfect for walks, jogs, runs and basking " +
+            "in the warm Mediterranean sun. With an exceptional view that extends from the city of Haifa " +
+            "to the distant white outcropping that is Rosh NaHikra, you can see the coastal cities of Nahariya, " +
+            "Akko (Acre) and the Krayot with the low, green mountains of the Western Galilee." +
+            "            <p>Watch the cargo ships come into the port and the naval ships dart to and fro with the " +
+            "slight chance of spotting dolphins leaping way down off the bay. On a clear day, the snowy peaks " +
+            "of Israel’s tallest mountain, Mount Hermon at 9,232 feet, can be seen as well. Obviously, telescopes " +
+            "and binoculars are advised and don’t forget your camera!" +
+            "            <p>The promenade is open 24 hours a day, 365 days a year and is free of charge."
+    },
+    {
+        title: "The Ursula Malbin`s Sculpture Garden",
+        src: "images/SculpturesGarden.jpg",
+        src_thumb: "images/thumb/SculpturesGarden_thumb.jpg",
+        alt: "Ursula Malbin`s Sculpture Garden",
+        description: "<h2>The Ursula Malbin`s Sculpture Garden</h2>\n" +
+            "            </p>Ursula Malbin was born on April 12, 1917, in Berlin to Jewish parents, both doctors of medicine. She herself was trained to become a cabinet-maker. In 1939 after her family had already left, a few weeks before World War II, she fled Nazi Germany, alone, penniless and without a passport.\n" +
+            "            <p>She was in Geneva when the war broke out, and there she met the sculptor Henri Paquet, whom she married in 1941. Ursula and Henri have a daughter Claudine born in 1945, who has two sons. Since 1967, Ursula Malbin has divided her creative life between the Artists’ Village of Ein Hod in Israel and the village of Troinex near Geneva in Switzerland.\n" +
+            "            <p>Malbin’s work is exceptional in its simplicity, in its fragile yet graceful depiction of the human form, and its celebration of the enduring beauty of life despite all vicissitudes. To maintain the purity of her inspiration, the artist works without models, relying only on her imagination for the pure, sculptured indication of her attitude to life.\n" +
+            "            <p>Over the years, Malbin’s works have found their way into Hospitals, Parks, Gardens, Schools and private collections in Europe and North America. One original of each of her creations is donated to the Sculpture Garden in Haifa that opened to the public in 1978.</p>"
+    },
+    {
+        title: "The Cave of Elijah",
+        src: "images/ElijahsCave.jpg",
+        src_thumb: "images/thumb/ElijahsCave_thumb.jpg",
+        alt: "Cave of Elijah",
+        description: "<h2>The Cave of Elijah</h2>\n" +
+            "            </p>Cave of Elijah is the name used for two grottoes on Mount Carmel, in Haifa, Israel, associated with Biblical prophet Elijah. According to tradition, Elijah is believed to have prayed at a grotto before challenging the priests of Baal on Mount Carmel (1 Kings 18), and to have hidden in either the same or in another nearby grotto from the wrath of Jezebel (1 Kings 19:1–3).</p>" +
+            "            </p>Two grottoes on Mount Carmel, in Haifa, have been historically referred to as \"the Cave of Elijah\". The main shrine known as the \"Cave of Elijah\" is located on Haifa's Allenby Road, on Mount Carmel, approximately 40 m above sea level. For centuries, it has been a destination for Jewish, Christian, Muslim and Druze pilgrims.[2] The Cave of Elijah in Allenby Road is divided into twos sections for praying, one for men and one for women; the cave is behind a velvet curtain.[3] The Cave is also known as el-Khader in Arabic.[4] The Druze regard it as holy,[2] and many among them identify Elijah as \"el-Khidr\", the green prophet who symbolizes water and life.</p>" +
+            "            </p>The cave has been considered by some as miracle-working. Sick people are said to be brought to the Cave in hope that they will be cured.</p>" +
+            "            </p>A second grotto, also associated with Elijah, is located nearby, under the altar of the main church of the Stella Maris Monastery, also on Mount Carmel.</p>"
+    },
+];
+
+setMainPicture();
+function setMainPicture(mainImageIndex) {
+    if (!mainImageIndex) mainImageIndex = 0;
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+    for (let i = 0; i < details.length; i++) {
+        let detail = details[i];
+        const img = document.createElement("img");
+        img.alt = detail.alt;
+        img.title = detail.title;
+        if (+mainImageIndex === +i) {
+            img.src = detail.src;
+            img.className = "main-image";
+            let divImage = document.querySelector(".big-image");
+            divImage.innerHTML = "";
+            divImage.appendChild(img);
+            let description = document.querySelector(".description");
+            description.innerHTML = detail.description;
+        } else {
+            let divImage = document.createElement("div");
+            divImage.className = "col";
+            img.src = detail.src_thumb;
+            img.className = "thumb-image";
+            img.setAttribute("data-index", i);
+            img.onclick = function() {setMainPicture(this.getAttribute("data-index"));};
+            divImage.appendChild(img)
+            gallery.appendChild(divImage);
+        }
+    }
+}
